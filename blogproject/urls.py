d42c0d6db6e37146bae3aeedda945e63976dad58
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2025-03-29 00:57:02 +0000

    add

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
     path("<slug:slug>/", Blogview.Blogs, name = "blogs"),
     path("dashboard/", include("dashboard.urls"))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
     handler400 = 'blogapp.views.bad_request'
