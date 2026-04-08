fd2e7da0885530a6cef1a86d06abf03526ded30a
user <user@Comrade-emf.local>
2026-04-08 18:59:58 +0100

    add

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 set -e
+
+echo "Switching to app directory..."
+cd /app
 
 echo "Installing Node dependencies..."
 npm install
@@ -10,3 +14,17 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Build complete!"
+
+
+# set -e
+
+# echo "Installing Node dependencies..."
+# npm install
+
+# echo "Building Tailwind CSS..."
+# npm run build
+
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+# echo "Build complete!"
