8f1b6192f537cc0d497cdec6ce5c4e50f6e3313b
user <user@Comrade-emf.local>
2026-04-09 11:49:46 +0100

    fix: rebuild tailwind css - add md:hidden

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./blogapp/templates/**/*.html"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 
 module.exports = {
   content: [
-    './templates/**/*.html',
-    './blogapp/templates/**/*.html',
-    './dashboard/templates/**/*.html',
-    './**/templates/**/*.html',
+    './**/*.html',
   ],
   theme: {
     extend: {},
